92dec873ed41c02fdf46638c91805dba0166ca6b
Alexander Krantz <klako@hotmail.se>
2022-02-16 15:27:08 +0100

    Inject code with mthod id, branch id, etc

--- src/main/java/org/coveraged/Main.java
+++ src/main/java/org/coveraged/Main.java
@@ -1,22 +1,20 @@
 package org.coveraged;
 
-import com.github.javaparser.JavaParser;
 import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.ConditionalExpr;
-import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.IntegerLiteralExpr;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.StringLiteralExpr;
 import com.github.javaparser.ast.stmt.*;
-import com.github.javaparser.ast.visitor.GenericVisitor;
-import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    private static MethodCallExpr myStmt = StaticJavaParser.parseExpression("CoverageRecorder.wrap(null, \"\")");
+    private static MethodCallExpr wrapExpr = StaticJavaParser.parseExpression("CoverageRecorder.wrap(null, \"\", 0)");
 
     public static void main(String[] args) throws Exception {
         //if (args.length != 1) {
@@ -28,21 +26,33 @@ public class Main {
 
         var cu = StaticJavaParser.parse(testClass);
 
-        modifyIf(cu);
-        modifyFor(cu);
-        modifyWhile(cu);
-        modifyDoWhile(cu);
-        modifyTernary(cu);
-
-        System.out.println(cu.toString());
+        cu.findAll(ClassOrInterfaceDeclaration.class).stream().forEach((foundClass) -> {
+            foundClass.findAll(MethodDeclaration.class).stream().forEach((method) -> {
+                var path = foundClass.getFullyQualifiedName().get();
+                modifyMethod(method, path);
+            });
+        });
 
         var writer = new FileWriter(testClass);
         writer.write(cu.toString());
         writer.close();
     }
 
-    private static void modifyIf(CompilationUnit cu) {
-        cu.findAll(IfStmt.class).stream().forEach((stmt) -> {
+    private static void modifyMethod(MethodDeclaration method, String path){
+        String name = path+"::"+method.getNameAsString();
+        AtomicInteger offset = modifyIf(method.getBody().get(), name, new AtomicInteger(0));
+        offset = modifyFor(method.getBody().get(), name, offset);
+        offset = modifyWhile(method.getBody().get(), name, offset);
+        offset = modifyDoWhile(method.getBody().get(), name, offset);
+        int count = modifyTernary(method.getBody().get(), name, offset).get();
+        MethodCallExpr initMethod = StaticJavaParser.parseExpression("CoverageRecorder.init(\"\", 0)");
+        initMethod.setArgument(0, new StringLiteralExpr(name))
+                .setArgument(1, new IntegerLiteralExpr(String.valueOf(count)));
+        method.getBody().get().addStatement(0, initMethod);
+    }
+
+    private static AtomicInteger modifyIf(BlockStmt methodBody, String methodId, AtomicInteger idOffset) {
+        methodBody.findAll(IfStmt.class).stream().forEach((stmt) -> {
             // Set then statement
             var thenStmt = stmt.getThenStmt();
             if (!thenStmt.isBlockStmt()) {
@@ -51,9 +61,11 @@ public class Main {
                 stmt.setThenStmt(block);
             }
             var thenBlock = stmt.getThenStmt().asBlockStmt();
-            if (!thenBlock.getStatements().getFirst().get().equals(myStmt)) {
-                var newStmt = myStmt.clone().setArgument(1, new StringLiteralExpr("if block"));
-                thenBlock.addStatement(0, newStmt);
+            var newWrapExpr = wrapExpr.clone()
+                    .setArgument(1, new StringLiteralExpr(methodId))
+                    .setArgument(2, new IntegerLiteralExpr(String.valueOf(idOffset.get())));
+            if (!thenBlock.getStatements().getFirst().get().asExpressionStmt().getExpression().equals(newWrapExpr)) {
+                thenBlock.addStatement(0, newWrapExpr);
                 stmt.setThenStmt(thenBlock);
             }
 
@@ -68,16 +80,22 @@ public class Main {
                     stmt.setElseStmt(block);
                 }
                 var elseBlock = stmt.getElseStmt().get().asBlockStmt();
-                if (!elseBlock.getStatements().getFirst().get().equals(myStmt)) {
-                    elseBlock.addStatement(0, myStmt);
+                idOffset.getAndIncrement();
+                newWrapExpr = wrapExpr.clone()
+                        .setArgument(1, new StringLiteralExpr(methodId))
+                        .setArgument(2, new IntegerLiteralExpr(String.valueOf(idOffset.get())));
+                if (!elseBlock.getStatements().getFirst().get().asExpressionStmt().getExpression().equals(newWrapExpr)) {
+                    elseBlock.addStatement(0, wrapExpr);
                     stmt.setElseStmt(elseBlock);
                 }
             }
+            idOffset.getAndIncrement();
         });
+        return idOffset;
     }
 
-    private static void modifyFor(CompilationUnit cu){
-        cu.findAll(ForStmt.class).stream().forEach((stmt) -> {
+    private static AtomicInteger modifyFor(BlockStmt methodBody,String methodId, AtomicInteger idOffset){
+        methodBody.findAll(ForStmt.class).stream().forEach((stmt) -> {
             var bodyStmt = stmt.getBody();
             if (!bodyStmt.isBlockStmt()) {
                 var block = StaticJavaParser.parseStatement("{}");
@@ -85,15 +103,20 @@ public class Main {
                 stmt.setBody(block);
             }
             var bodyBlock = stmt.getBody().asBlockStmt();
-            if (!bodyBlock.getStatements().getFirst().get().equals(myStmt)) {
-                bodyBlock.addStatement(0, myStmt);
+            var newWrapExpr = wrapExpr.clone()
+                    .setArgument(1, new StringLiteralExpr(methodId))
+                    .setArgument(2, new IntegerLiteralExpr(String.valueOf(idOffset.get())));
+            if (!bodyBlock.getStatements().getFirst().get().asExpressionStmt().getExpression().equals(newWrapExpr)) {
+                bodyBlock.addStatement(0, newWrapExpr);
                 stmt.setBody(bodyBlock);
             }
+            idOffset.getAndIncrement();
         });
+        return idOffset;
     }
 
-    private static void modifyWhile(CompilationUnit cu){
-        cu.findAll(WhileStmt.class).stream().forEach((stmt) -> {
+    private static AtomicInteger modifyWhile(BlockStmt methodBody,String methodId, AtomicInteger idOffset){
+        methodBody.findAll(WhileStmt.class).stream().forEach((stmt) -> {
             var bodyStmt = stmt.getBody();
             if (!bodyStmt.isBlockStmt()) {
                 var block = StaticJavaParser.parseStatement("{}");
@@ -101,15 +124,20 @@ public class Main {
                 stmt.setBody(block);
             }
             var bodyBlock = stmt.getBody().asBlockStmt();
-            if (!bodyBlock.getStatements().getFirst().get().equals(myStmt)) {
-                bodyBlock.addStatement(0, myStmt);
+            var newWrapExpr = wrapExpr.clone()
+                    .setArgument(1, new StringLiteralExpr(methodId))
+                    .setArgument(2, new IntegerLiteralExpr(String.valueOf(idOffset.get())));
+            if (!bodyBlock.getStatements().getFirst().get().asExpressionStmt().getExpression().equals(newWrapExpr)) {
+                bodyBlock.addStatement(0, newWrapExpr);
                 stmt.setBody(bodyBlock);
             }
+            idOffset.getAndIncrement();
         });
+        return idOffset;
     }
 
-    private static void modifyDoWhile(CompilationUnit cu){
-        cu.findAll(DoStmt.class).stream().forEach((stmt) -> {
+    private static AtomicInteger modifyDoWhile(BlockStmt methodBody,String methodId, AtomicInteger idOffset){
+        methodBody.findAll(DoStmt.class).stream().forEach((stmt) -> {
             var bodyStmt = stmt.getBody();
             if (!bodyStmt.isBlockStmt()) {
                 var block = StaticJavaParser.parseStatement("{}");
@@ -117,16 +145,41 @@ public class Main {
                 stmt.setBody(block);
             }
             var bodyBlock = stmt.getBody().asBlockStmt();
-            if (!bodyBlock.getStatements().getFirst().get().asExpressionStmt().getExpression().equals(myStmt)) {
-                bodyBlock.addStatement(0, myStmt);
+            var newWrapExpr = wrapExpr.clone()
+                    .setArgument(1, new StringLiteralExpr(methodId))
+                    .setArgument(2, new IntegerLiteralExpr(String.valueOf(idOffset.get())));
+            if (!bodyBlock.getStatements().getFirst().get().asExpressionStmt().getExpression().equals(newWrapExpr)) {
+                bodyBlock.addStatement(0, newWrapExpr);
                 stmt.setBody(bodyBlock);
             }
+            idOffset.getAndIncrement();
         });
+        return idOffset;
     }
 
-    private static void modifyTernary(CompilationUnit cu){
-        cu.findAll(ConditionalExpr.class).stream().forEach((expr) -> {
-            System.out.println(expr);
+    private static AtomicInteger modifyTernary(BlockStmt methodBody, String methodId, AtomicInteger idOffset){
+        methodBody.findAll(ConditionalExpr.class).stream().forEach((expr) -> {
+            var thenExpr = expr.getThenExpr();
+            var elseExpr = expr.getElseExpr();
+            var newWrapExpr = wrapExpr.clone()
+                    .setArgument(1, new StringLiteralExpr(methodId))
+                    .setArgument(2, new IntegerLiteralExpr(String.valueOf(idOffset.get())));
+            if (!thenExpr.isMethodCallExpr()) {
+                thenExpr = newWrapExpr.setArgument(0,thenExpr);
+                expr.setThenExpr(thenExpr);
+            } else if(!thenExpr.asMethodCallExpr().getName().equals(wrapExpr.getName())) {
+                thenExpr = newWrapExpr.setArgument(0,thenExpr);
+                expr.setThenExpr(thenExpr);
+            }
+            if (!elseExpr.isMethodCallExpr()) {
+                elseExpr = newWrapExpr.setArgument(0, elseExpr);
+                expr.setElseExpr(elseExpr);
+            } else if (!elseExpr.asMethodCallExpr().getName().equals(wrapExpr.getName())) {
+                elseExpr = newWrapExpr.setArgument(0, elseExpr);
+                expr.setElseExpr(elseExpr);
+            }
+            idOffset.getAndIncrement();
         });
+        return idOffset;
     }
 }
